036987de2093250cfcae2cde38ea24d474ff7ce3
LudaFrolenko <ddreddln@gmail.com>
2023-03-24 13:50:16 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(555);
+        System.out.println(777);
+        System.out.println(333);
+    }
+}
